e1c606c58b09e255d69b8a3b5cbb25889cfa5f99
SaranyaSV16 <saraveer5801@gmail.com>
2024-02-29 16:49:20 +0530

    Second Commit

---
+++ src/test/java/com/edex/recime/RecimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edex.recime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
